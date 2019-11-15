ea35f13c41f20d1a0c6b7ac1fcf2527eb75cfb66
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-15 18:51:02 +0530

    Add code for feature ladder snake and noplay

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,10 +1,31 @@
   echo "Welcome to  the program Snakeladder"
-	
-	#constants
-   position=0
-	
+
+	#constants  
+	NOPLAY=0
+	SNAKE=1
+	LADDER=2
+	#variable
+	position=0
 	function rollingDie()
-	{
+   {
 		dieValue=$((RANDOM%6+1))
 	}
+	
 	rollingDie
+
+	function playingOption()
+	{
+		playChoice=$((RANDOM%3))
+
+		case $playChoice in $NOPLAY )
+				possition=0;;
+
+				$LADDER )
+				position=$(( $position+$dieValue ));;
+
+				$SNAKE )
+				position=$(( $position+$dieValue ));;
+		esac
+	}
+
+	playingOption
